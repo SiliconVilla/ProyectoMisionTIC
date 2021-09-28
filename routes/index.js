5b15ad8e0153ecbd0f9126902ba39f4171393b38
Jayder Johany Cabezas Molina <90657836+SiliconVilla@users.noreply.github.com>
2021-09-28 04:49:04 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const { Router} = require('express');
 const router = Router();
 const admin = require('firebase-admin');
 
-var serviceAccount = require("../nodejs-a846c-firebase-adminsdk-ij0gw-dba4f09f3c.json");
+var serviceAccount = require("../nodefb-32e8e-firebase-adminsdk-7kv25-511e2477e8.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://nodejs-a846c-default-rtdb.firebaseio.com/'
+    databaseURL: 'https://nodefb-32e8e-default-rtdb.firebaseio.com/'
 });
 
 const db = admin.database();
@@ -15,8 +15,14 @@ const db = admin.database();
 //Variante para el router, declarando previamente express como const
 //const router = express.Router();
 
+//Listar los productos de la base de datos
 router.get('/', (req, res) => {
-    res.render('productos/listado');
+    db.ref('productos').once('value', (snapshot) => {
+        const data = snapshot.val();
+        res.render('productos/listado', { productos: data });
+        console.log(data);
+    });
+    
 })
 
 module.exports = router;
